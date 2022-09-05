226a95cf705e2bea8e127a9165934e1d5632d60d
tharindudarsh <darshanaruh@gmail.com>
2022-09-05 17:11:04 +0530

    close #8

--- src/main/java/lk/ijse/dep9/controller/AdminDashBoardController.java
+++ src/main/java/lk/ijse/dep9/controller/AdminDashBoardController.java
@@ -1,5 +1,20 @@
 package lk.ijse.dep9.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class AdminDashBoardController {
 
+    public Button btnProfileManagement;
+    public Button btnViewRecords;
+    public Button btnSetting;
+
+    public void btnProfileManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnViewRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSettingOnAction(ActionEvent actionEvent) {
+    }
 }
---
+++ src/main/java/lk/ijse/dep9/controller/ProfileManagementController.java
@@ -0,0 +1,21 @@
+package lk.ijse.dep9.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class ProfileManagementController {
+    public TableView tblProfiles;
+    public Button btnAddProfile;
+    public Button btnModifyProfile;
+    public Button btnDeleteProfile;
+
+    public void btnAddProfileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnModifyProfileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteProfileOnAction(ActionEvent actionEvent) {
+    }
+}
