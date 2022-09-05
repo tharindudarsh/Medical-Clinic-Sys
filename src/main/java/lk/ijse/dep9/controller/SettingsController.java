0cf9f8890020617454a8841b3c730e4bfb342377
tharindudarsh <darshanaruh@gmail.com>
2022-09-05 17:44:45 +0530

    close #9

---
+++ src/main/java/lk/ijse/dep9/controller/SettingsController.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep9.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class SettingsController {
+    public Button btnManageFields;
+    public Button btnAddDiscount;
+    public Button btnChangePassword;
+    public Button btnAbout;
+    public Button btnManageFees;
+    public AnchorPane ankprnInSettings;
+
+    public void btnManageFieldsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnChangePasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutonAction(ActionEvent actionEvent) {
+    }
+
+    public void btnManageFeesOnAction(ActionEvent actionEvent) {
+    }
+}
